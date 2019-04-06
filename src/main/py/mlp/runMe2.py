8c5d0e2fd6ee7ee1fa4c64f049013bc6a97627bc
Harald-W <harald.witte@students.unibe.ch>
2019-04-06 17:04:50 +0200

    MLP - grid search for parameter optimization

---
+++ src/main/py/mlp/runMe2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import numpy as np
+import os
+
+if __name__ == '__main__':
+    reportFileName = 'MLP_test_report_out_csv.txt'
+
+    reportFile = open(reportFileName, 'w+')
+    reportFile.write('***** MLP parameter test - MNIST data set - 10 epochs per parameter combination***** \n')
+    reportFile.write('final_accuracy_%, batch_size, learning_rate, hidden_width, dropout' + '\n')
+    reportFile.close()
+    i=0
+    print("Parameter optimization started \n   Creating report...")
+    for batchSize in [64, 128, 192]:
+        for learningRate in [1e-3, 3e-3]:
+            for dropout in [1, 0.9, 0.8]:
+                for hiddenWidth in [512, 1024]:
+                    i+=1
+                    print("Parameter combination", i, "out of 36")
+                    #reportFile = open(reportFileName, 'a')
+                    #reportFile.write('batch_size\t'+str(batchSize)+'\tlearning_rate\t'+str(learningRate)+'\thidden_width\t'+str(hiddenWidth)+'\tdropout\t'+str(dropout)+'\n')
+                    #reportFile.close()
+                    cmd = 'python mlp_1.0.py --batch_size ' + str(batchSize) +' --learning_rate ' + str(learningRate) +' --hidden_width ' + str(hiddenWidth) +' --dropout ' + str(dropout) +' >> ' + reportFileName
+                    os.system(cmd)
+                    reportFile = open(reportFileName, 'a')
+                    reportFile.write(', ' + str(batchSize) + ', ' + str(
+                        learningRate) + ', ' + str(hiddenWidth) + ', ' + str(dropout) + '\n')
+                    reportFile.close()
