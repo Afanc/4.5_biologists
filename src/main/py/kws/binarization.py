ae6962c2c0f6a6d4cffa68eee42b43ab72e88587
waljan <jannis.wallau@students.unibe.ch>
2019-04-11 15:01:05 +0200

    better way of binarization (locally adapted threshold)

--- src/main/py/kws/binarization.py
+++ src/main/py/kws/binarization.py
@@ -11,6 +11,7 @@ import scipy.misc
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from skimage.filters import threshold_local
 
 #path of the folder containing the original images
 input_path = 'src/main/py/kws/data/images'
@@ -27,10 +28,16 @@ list_of_images = os.listdir(input_path)
 for file in list_of_images:
     # load image
     image = plt.imread(input_path + '/' + file)
-    # set threshold for binarization (you can try different ones)
-    threshold = image.max()*0.76
-    #binarization (every pixel aboce the threshold will be set to one and all others to 0
-    image = np.where(image > threshold,1, 0)
+
+    # # set threshold for binarization (you can try different ones)
+    # threshold = image.max()*0.76
+    # #binarization (every pixel aboce the threshold will be set to one and all others to 0
+    # image = np.where(image > threshold,1, 0)
+
+    # specify window (101x101) for which the threshold will be adapted to
+    block_size = 101
+    # use threshold adapted to local neighborhood, every pixel above the threshold will be set to 1, all others are set to 0
+    binary_image = np.where(image > threshold_local(image, block_size, offset=10), 1, 0)
     #save the processed file
-    scipy.misc.imsave(output_path + '/' + file, image)
+    scipy.misc.imsave(output_path + '/' + file, binary_image)
 
