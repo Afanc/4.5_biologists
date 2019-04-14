9c2018fb262afe00be0ada8c5b457a5c2edeef8f
Sven-Wa <sven.wallau@students.unibe.ch>
2019-04-14 13:52:10 +0200

    this script resize images in the word_images folder

--- src/main/py/kws/resize_images.py
+++ src/main/py/kws/resize_images.py
@@ -13,10 +13,17 @@ if not os.path.exists(output_path):
 
 list_of_words = os.listdir(input_path)
 
-for file in list_of_words:
-    img = plt.imread(input_path + '/' + file)
 
-    resized_img = cv2.resize(img, (100, 100))
+
+for file in list_of_words:
+    img = plt.imread(input_path + '/' + file) #the loaded image has the shape: (height, widht, 4)
+
+    #these steps remove the 4 channels in the 3. dimension
+    frame = img[:, :, 3]
+    img = img[:, :, 0]
+    img = np.where((frame[:, :] == 0), 1, img[:, :])
+
+    resized_img = cv2.resize(img, (100, 100)) #the reshaped image has the shape: (100, 100)
 
     scipy.misc.imsave(output_path + '/' + file, resized_img)
 
