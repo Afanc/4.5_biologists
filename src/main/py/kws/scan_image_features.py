380408ab7a5bd21a9bb2f6c3f98cbbd5d9b770bf
Harald-W <harald.witte@students.unibe.ch>
2019-04-14 16:40:41 +0200

    option for normalization included (from Jannis' function)

--- src/main/py/kws/scan_image_features.py
+++ src/main/py/kws/scan_image_features.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageDraw
 # img = Image.open("270-25-05_Clock.png")
 
 
-def scan_image_features(image_file, no_of_features):
+def scan_image_features(image_file, normalize_matrix = False):
     """Scans image row-wise and returns vector of dimensions no_of_features x image width
     with features for each column."""
     img = Image.open(image_file)
@@ -12,11 +12,17 @@ def scan_image_features(image_file, no_of_features):
     img_array = np.array(img)
     img_height = img_array.shape[0]
     img_width = img_array.shape[1]
-    feature_vector = np.zeros(shape = (no_of_features, img_width))  #, dtype = uint8)
+    no_of_features = len( feature_extraction(img_array[:, 1].reshape(img_height, 1)) )  # get number of features assessed by feature_extraction-function
+    feature_matrix = np.zeros(shape = (no_of_features, img_width))
     for column in range(img_width):
         col = img_array[:, column].reshape(img_height, 1)
         col_features = feature_extraction(col)
-        feature_vector[ : , column] = col_features
-    return feature_vector
+        feature_matrix[ : , column] = col_features
+    if normalize_matrix:
+        feature_matrix = normalization(feature_matrix)
+    return feature_matrix
 
-# features_Clock = scan_image_features("270-25-05_Clock.png", 4)
+# features_Clock = scan_image_features("270-25-05_Clock.png", normalize_matrix=False)
+# same as:
+# features_Clock2 = scan_image_features("270-25-05_Clock.png")
+# features_Clock_norm = scan_image_features("270-25-05_Clock.png", normalize_matrix=True)
