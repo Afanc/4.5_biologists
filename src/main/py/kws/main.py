c14aeafc070bfd0723d51f744ac97c50488855c2
Afanc <dariush.mollet@gmail.com>
2019-04-18 08:58:10 +0200

    should be better

--- src/main/py/kws/main.py
+++ src/main/py/kws/main.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
 
-import os
+import os, numpy as np
 import argparse
-import crop_svg_outline as crop
-import resize_images as res
-import read_SVG
-import read_transcription as read_trans
-import crop_svg_outline as c_s_o
+import read_transcription as rt
+import read_SVG as svgread
+import crop_svg_outline as svgcrop
+import binarization as binary
+import feature_extraction as features
+import scan_image_features as scan
+from matplotlib import pyplot as plt
+from PIL import Image
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--preprocessing', default=True, type=bool)
 parser.add_argument('--id_linking', default=True, type=bool)
 args = parser.parse_args()
-
 
 #----- paths and folders and shit ----#
 work_dir = os.getcwd()
@@ -23,15 +26,11 @@ if(work_dir[-14:] != "4.5_biologists" and work_dir[-3:] != "kws"):
 paths= {}
 
 #directories
-paths["images_input"] = 'data/images'
+paths["images"] = 'data/images'
 paths["images_output"] = 'data/binarized_images'
 paths["wordimages_input"] = "data/word_images"
 paths["wordimages_output"] = "data/resized_word_images"
 paths["svg"] = "data/ground-truth/locations"
-
-for p in paths :
-    if not os.path.exists(p):
-        os.makedirs(p)
 
 #files
 paths["transcription.txt"] = "data/ground-truth/transcription.txt"
@@ -41,39 +40,66 @@ if(os.getcwd()[-14:] == "4.5_biologists"):
     for k in paths :
         paths[k] = "src/main/py/kws/" + paths[k]
 
+#adapt for fucking windows
+if (os.uname().sysname == "Windows") :
+    for k in paths :
+        paths[k] = re.sub("/", "\\\\", paths[k])
+        #or
+        #p = os.path.normpath(p) ?
+
+#and create directories if these don't exist
+for k in paths :
+    if (not os.path.exists(paths[k]) and paths[k][:-4] != ".txt"):
+        os.makedirs(p)
 
 
 list_of_wordimages = os.listdir(paths["wordimages_input"])
+list_of_svg = os.listdir(paths["svg"])
+
+list_of_images = os.listdir(paths["images"])
 list_of_svg = os.listdir(paths["svg"])
 
 
 #----- ID linking----#
 if args.id_linking :
 
-    for filename in list_of_svg:
-        svgfile= paths["svg"]+"/"+filename
-        imagefile=paths["images_input"]+"/"+filename[:-4]+".jpg"
-
-        coord_list = read_SVG.extract_SVG_masks(svgfile)
-
-        word_dic = read_trans.read_transcription(file_name=paths["transcription.txt"], output = "word_dict")
-
-        ID_dict = read_trans.retrieve_IDs("General", word_dic)
-
-#this does not work for whatever reason
-        c_s_o.crop_svg_outline(imagefile, ID_dict = ID_dict, svg_coordinates = coord_list)
-
-        generate_word_ID_csv(word_dict = word_dic, file_name = "word_IDs.txt", sep = ",")
-
-        break
-
-exit()
+    word_dict = rt.read_transcription(file_name = paths["transcription.txt"], output = "word_dict")
+    ID_dict =  rt.read_transcription(file_name = paths["transcription.txt"], output = "ID_dict")
 
 
 #----- pre-processing ----#
 if args.preprocessing :
 
-    for image in list_of_images :
+    i = 0
+    for page_no, page in enumerate(list_of_images):
+        print("processing page ", i, " out of ", len(list_of_images))
+        svg = os.path.normpath(os.path.join(paths["svg"], page[:-4]+".svg"))
+        image = os.path.normpath(os.path.join(paths["images"], page))
+        coord_list = svgread.extract_SVG_masks(svg)
+        svgcrop.crop_svg_outline(image, ID_dict = ID_dict, svg_coordinates = coord_list)
+        
 
-        res.resize_images
-#blabla
+    list_of_wordimages = os.listdir(paths["wordimages_input"])
+    word_lengths = [len(word) for word in word_dict]
+    median_word_length = int(np.median(word_lengths))
+    
+    i = 0
+    for file in list_of_wordimages:
+        print("processing page ", i, " out of ", len(list_of_images))
+        file_in = os.path.normpath(os.path.join(paths["wordimages_input"], file))
+        img = plt.imread(file_in) #the loaded image has the shape: (height, widht, 4)
+
+     #these steps remove the 4 channels in the 3. dimension
+        frame = img[:, :, 3]
+        img = img[:, :, 0]
+        img = np.where((frame[:, :] == 0), 1, img[:, :])
+#        img = plt.imread("270-25-05_Clock.png")
+        img_PIL = Image.fromarray(img)
+        resized_img = img_PIL.resize(size = (100, 200)) #the reshaped image has the shape: (100, 100)
+        resized_img_array = np.array(resized_img)
+        file_out = os.path.normpath(os.path.join(paths["wordimages_output"], file))
+#        resized_img.save(file_out)
+#        scipy.misc.imsave(file_out, resized_img)
+        resized_img_bin = binary.binarize_image(resized_img_array, block_size = 5)
+        binary.save_image_png(file_out, resized_img_bin)
+
