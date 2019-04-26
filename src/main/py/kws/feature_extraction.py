f01d1d92caf66ce4d3f682462e5f578f3cc3bdf3
Harald-W <harald.witte@students.unibe.ch>
2019-04-27 00:07:44 +0200

    New features added - please test is beneficial

--- src/main/py/kws/feature_extraction.py
+++ src/main/py/kws/feature_extraction.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import numpy as np
+import sys
+import random
 import matplotlib.pyplot as plt
 
-def feature_extraction(col, num_f):
+def feature_extraction(col, num_f = 9):
     """
     input: column of a image
     output: feature vector containing the different feature values of this column
@@ -12,6 +14,11 @@ def feature_extraction(col, num_f):
         upper_boundary
         lower_boundary
         black_white_transitions
+        percentage of black pixels between upper and lower bound
+        percentage of black pixels in center half
+        percentage of black pixels in lower third
+        percentage of black pixels in lower quarter
+        percentage of black pixels in upper third
         ...?
     """
     # update this if you add a feature
@@ -28,33 +35,52 @@ def feature_extraction(col, num_f):
             # when there is a transition from black to white or reverse, the counter is increased by one
             if col[row] != col[row+1]:
                 black_white_transitions += 1
+        if (lower_boundary - upper_boundary) == 0:
+            perc_upper_lower = 1
+        else:
+            perc_upper_lower = np.sum(col[upper_boundary:lower_boundary+1] == 0) / (lower_boundary - upper_boundary)
+        perc_center_half = np.sum(col[len(col)//4 : -len(col)//4] == 0) / (len(col)/2)
+        perc_lower_third = np.sum(col[ -len(col)//3 : ] == 0) / (len(col)/3)
+        perc_lower_quarter = np.sum(col[-len(col)//4 : ] == 0) / (len(col)/4)
+        perc_upper_third = np.sum(col[ : len(col)//3] == 0) / (len(col)/3)
     # if there are no black pixels the features get the following values
     else:
         upper_boundary = len(col)
         lower_boundary = len(col)
-        black_white_transitions = 0  # this will cause errors on a pure black or pure white image, but as soon as there is a single black-white transition in one columns of the image, it should work
-    feature_values = np.array([number_of_black_pixels, upper_boundary, lower_boundary, black_white_transitions]).reshape(num_of_features, )
+        black_white_transitions = random.choice([0, sys.float_info.epsilon])  # this will cause errors on a pure black or pure white image, but as soon as there is a single black-white transition in one columns of the image, it should work
+        perc_upper_lower = random.choice([0, sys.float_info.epsilon])
+        perc_center_half = random.choice([0, sys.float_info.epsilon])
+        perc_lower_third = random.choice([0, sys.float_info.epsilon])
+        perc_lower_quarter = random.choice([0, sys.float_info.epsilon])
+        perc_upper_third = random.choice([0, sys.float_info.epsilon])
+    feature_values = np.array([number_of_black_pixels, upper_boundary, \
+                               lower_boundary, black_white_transitions, \
+                               perc_upper_lower, perc_center_half, \
+                               perc_lower_third, perc_lower_quarter, \
+                               perc_upper_third]).reshape(num_of_features, )
     return feature_values
 
 
-def get_feature_vectors(img):
+def get_feature_vectors(img, num_f):
     """
     takes an image as input
     computes the feature vectors for the different features
     returns a matrix of the feature vectors (each row corresponds to a feature vector
     """
-    feature_matrix = np.zeros(shape = (4, img.shape[1]))
+    num_of_features = num_f
+    feature_matrix = np.zeros(shape = (num_of_features, img.shape[1]))
     for col in range(img.shape[1]):
         feature_matrix[:, col] = feature_extraction(img[:,col].reshape(img.shape[0],1))
     return feature_matrix
 
 
-def normalization(feature_matrix):
+def normalization(feature_matrix, num_f):
     """
     takes a feature matrix as imput: each row corresponds to the feature vector of a image for one particular feature
     returns a normalized feature matrix  (xi-mean)/sd
     """
-    for row in range(4):
+    num_of_features = num_f
+    for row in range(num_of_features):
         feature_matrix[row, :] = (feature_matrix[row, :] - np.mean(feature_matrix[row, :])) / np.std(feature_matrix[row,:])
     return feature_matrix
 
