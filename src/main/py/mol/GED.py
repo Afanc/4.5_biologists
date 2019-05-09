e908beef0f9cb79b85fd931efcf1a2fdc98f75fc
waljan <jannis.wallau@students.unibe.ch>
2019-05-09 16:29:26 +0200

    ged

---
+++ src/main/py/mol/GED.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import graph_representation as gr
+
+
+# create instances for all molecules
+input_path = "data/gxl"
+adj_matrices = gr.adj_matrix(input_path)
+# for i in adj_matrices["16.gxl"]:
+#     print(i)
+# print(M16.get_adj_matrix())
+
+def GED(g1, g2):
+    pass
+
+
